ef096c4b45dc926323b3565c9a563cfce77cabce
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2021-10-16 20:29:55 +0200

    Add libaslrmallocrun, a convenience program to LD_PRELOAD libaslrmalloc
    Inspired by gamemoderun but implemented in C to make usage in sandboxes
    without a shell easier/possible (e.g. firejail --private-bin=libaslrmallocrun
    --profile=gnome-calendar libaslrmallocrun gnome-calendar).

---
+++ libaslrmallocrun.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later OR BSD-3-Clause
+
+#define _GNU_SOURCE
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef LIBASLRMALLOC
+#error LIBASLRMALLOC not defined
+#endif
+
+void usage() {
+	printf("USAGE:");
+	printf("  libaslrmallocrun <PROGRAM> [ARGUMENTS]");
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		usage();
+		return 1;
+	}
+
+	char *new_ld_preload = LIBASLRMALLOC;
+	const char *ld_preload = getenv("LD_PRELOAD");
+	if (ld_preload != NULL) {
+		if (asprintf(&new_ld_preload, "%s %s", ld_preload, LIBASLRMALLOC) == -1)
+			abort();
+	}
+	if (setenv("LD_PRELOAD", new_ld_preload, 1) != 0)
+		abort();
+
+	if (execvp(argv[1], &argv[1]) == -1) {
+		printf("libaslrmallocrun: Failed to start '%s': %s\n", argv[1], strerror(errno));
+		return 1;
+	}
+}
